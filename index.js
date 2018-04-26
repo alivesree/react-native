bc7ae37a8228f53c1c6934bb8ce7440e9641b71a
Sreehari <sreeharikg@live.com>
2018-04-26 22:50:52 +0530

    1st push

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text,AppRegistry} from 'react-native';
+
+const App=()=>{
+    return(
+        <Text> test </Text>
+    );
+};
+AppRegistry.registerComponent('albums',()=>App);
